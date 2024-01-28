58bc6886532660cf46e8246e548dde3819740072
Anmisyeah <andrew.mironov.1426@gmail.com>
2024-01-28 23:50:41 +0600

    feat: project is completed

---
+++ src/scripts/script.js
@@ -0,0 +1,47 @@
+
+let scrollingElement = document.querySelector('.header');
+      
+        function handleScroll() {
+
+          var scrollPosition = window.scrollY;
+      
+
+          if (scrollPosition > 0) {
+            scrollingElement.classList.add('scrolled');
+          } else {
+            scrollingElement.classList.remove('scrolled');
+          }
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+
+        let burgerButton = document.querySelector('.burger');
+
+        burgerButton.addEventListener('click', function() {
+          this.classList.toggle('active')
+          console.log(this)
+          document.querySelector('.header__nav').classList.toggle('open')
+        });
+
+        let headerLinks = document.querySelectorAll('.header__link');
+
+        headerLinks.forEach(function(link) {
+          link.addEventListener('click', function() {
+            // Закрываем меню
+            burgerButton.classList.remove('active');
+            document.querySelector('.header__nav').classList.remove('open');
+          });
+        });
+
+
+
+
+var swiper = new Swiper(".sliderSwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+console.log('123')
